877615586c48ccd0f09e0cdd0cf6025cd91877cc
aaronjohnston4 <aaron.johnston4@gmail.com>
2023-01-28 21:31:27 -0600

    adding all changes made

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Designs(models.Model):
+
+    title = models.CharField(max_length=100)
+    img = models.CharField(max_length=250)
+    bio = models.TextField(max_length=500)
+    # verified_products = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
+from .models import Designs
+from django.views.generic import DetailView
 
 # Create your views here.
 class Home(TemplateView):
@@ -11,7 +13,7 @@ class Home(TemplateView):
     #     return HttpResponse("movu4 Home")
 
 
-class design_detail(DetailView)
+class DesignDetail(DetailView):
     model = Designs
     template_name = "design_detail.html"
 
